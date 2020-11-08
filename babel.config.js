b6cc0a0d0b7f5459bd22a61561ab4a4d18d7fd6e
MisterTicot <mister.ticot@cosmic.plus>
2020-11-08 23:20:37 +0100

    dev Change: tweak browser support

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,12 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
-    presets: [["@babel/preset-env", { useBuiltIns: "usage", corejs: 3 }]],
+    presets: [
+      [
+        "@babel/preset-env",
+        { useBuiltIns: "usage", corejs: 3, exclude: ["es.promise"] }
+      ]
+    ],
     plugins: [
       "@babel/plugin-transform-runtime",
       "@babel/plugin-syntax-dynamic-import"
